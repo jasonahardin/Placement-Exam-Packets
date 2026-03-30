3706a36a49b119c0e4d0e0f8531ab79c63729948
Jason Hardin <jhardin@worcester.edu>
2026-03-30 19:14:21 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,419 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "study-guides-3",
+  "level": "1",
+  "url": "study-guides-3.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Arithmetic Study Guide",
+  "body": " Arithmetic Study Guide    Disclaimer: This study guide is intended to assist in preparation for the Arithmetic Placement Test at Worcester State University by providing practice problems on the various topics covered on the test, as well as skills needed in all courses beyond MA-098:Developmental Arithmetic. Please note that problems on the test will be different than those presented here. Answers to all problems appear at the end of this packet. If you require any assistance, free drop-in tutoring is available at the WSU Math Center (Sullivan 140) during the fall and spring semesters. Stop by Sullivan 140 to see the current schedule.   Note: Students may not use calculators during the test.     Operations with Whole Numbers   These operations include addition, subtraction, multiplication, and division.                              meters meters     meters                                                              R         Operations with Fractions   Fraction skills are very important in all mathematics classes. These include basic operations and the ability to solve problems using fractions, decimals and percents. Understanding prime factors, reducing fractions, conversion between mixed and improper fractions, and finding equivalent fractions are all basic skills for fractions. If you lack any of these basic skills or need a review, we recommend you visit the WSU Math Center.                                                                                                           Operations with Decimals   Operations with decimals skills including basic operations of addition, subtraction, multiplication and division.                                                              Simplifying Algebraic Expressions   These problems require \"PEMDAS\" or order of operations. Remember multiplication and division are equal and you work from left to right. For example, , not                                          and       Solving Simple Linear Equations and Proportions   For this test, you need to be able to solve simple linear equations like the ones shown.                                 Converting Percents, Fractions, and Decimals   These conversions can appear as a standalone problem or within another problem. Remember it is usually advisable to convert all to decimals or all to fractions before simplifying an expression or equation.     Express as a decimal and a percent.      Express as a fraction and a percent.      Express as a fraction and a decimal.     "
+},
+{
+  "id": "study-guides-3-3-3",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "           "
+},
+{
+  "id": "study-guides-3-3-4",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "           "
+},
+{
+  "id": "study-guides-3-3-5",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "   meters meters     meters   "
+},
+{
+  "id": "study-guides-3-3-6",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "           "
+},
+{
+  "id": "study-guides-3-3-7",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-7",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "           "
+},
+{
+  "id": "study-guides-3-3-8",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-8",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "           "
+},
+{
+  "id": "study-guides-3-3-9",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-9",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "           "
+},
+{
+  "id": "study-guides-3-3-10",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-10",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-3-11",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-3-11",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": "   R    "
+},
+{
+  "id": "study-guides-3-4-3",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-3",
+  "type": "Worksheet Exercise",
+  "number": "10",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-4",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-4",
+  "type": "Worksheet Exercise",
+  "number": "11",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-5",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-5",
+  "type": "Worksheet Exercise",
+  "number": "12",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-6",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-6",
+  "type": "Worksheet Exercise",
+  "number": "13",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-7",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-7",
+  "type": "Worksheet Exercise",
+  "number": "14",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-8",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-8",
+  "type": "Worksheet Exercise",
+  "number": "15",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-9",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-9",
+  "type": "Worksheet Exercise",
+  "number": "16",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-10",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-10",
+  "type": "Worksheet Exercise",
+  "number": "17",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-11",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-11",
+  "type": "Worksheet Exercise",
+  "number": "18",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-12",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-12",
+  "type": "Worksheet Exercise",
+  "number": "19",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-14",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-14",
+  "type": "Worksheet Exercise",
+  "number": "20",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-15",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-15",
+  "type": "Worksheet Exercise",
+  "number": "21",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-16",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-16",
+  "type": "Worksheet Exercise",
+  "number": "22",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-4-17",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-4-17",
+  "type": "Worksheet Exercise",
+  "number": "23",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-3",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-3",
+  "type": "Worksheet Exercise",
+  "number": "24",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-4",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-4",
+  "type": "Worksheet Exercise",
+  "number": "25",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-5",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-5",
+  "type": "Worksheet Exercise",
+  "number": "26",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-6",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-6",
+  "type": "Worksheet Exercise",
+  "number": "27",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-7",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-7",
+  "type": "Worksheet Exercise",
+  "number": "28",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-8",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-8",
+  "type": "Worksheet Exercise",
+  "number": "29",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-9",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-9",
+  "type": "Worksheet Exercise",
+  "number": "30",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-5-10",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-5-10",
+  "type": "Worksheet Exercise",
+  "number": "31",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-6-3",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-6-3",
+  "type": "Worksheet Exercise",
+  "number": "32",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-6-4",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-6-4",
+  "type": "Worksheet Exercise",
+  "number": "33",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-6-5",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-6-5",
+  "type": "Worksheet Exercise",
+  "number": "34",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-6-6",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-6-6",
+  "type": "Worksheet Exercise",
+  "number": "35",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-6-7",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-6-7",
+  "type": "Worksheet Exercise",
+  "number": "36",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-6-8",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-6-8",
+  "type": "Worksheet Exercise",
+  "number": "37",
+  "title": "",
+  "body": "   and    "
+},
+{
+  "id": "study-guides-3-7-3",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-7-3",
+  "type": "Worksheet Exercise",
+  "number": "38",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-7-4",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-7-4",
+  "type": "Worksheet Exercise",
+  "number": "39",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-7-5",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-7-5",
+  "type": "Worksheet Exercise",
+  "number": "40",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-7-6",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-7-6",
+  "type": "Worksheet Exercise",
+  "number": "41",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "study-guides-3-8-3",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-8-3",
+  "type": "Worksheet Exercise",
+  "number": "42",
+  "title": "",
+  "body": "  Express as a decimal and a percent.   "
+},
+{
+  "id": "study-guides-3-8-4",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-8-4",
+  "type": "Worksheet Exercise",
+  "number": "43",
+  "title": "",
+  "body": "  Express as a fraction and a percent.   "
+},
+{
+  "id": "study-guides-3-8-5",
+  "level": "2",
+  "url": "study-guides-3.html#study-guides-3-8-5",
+  "type": "Worksheet Exercise",
+  "number": "44",
+  "title": "",
+  "body": "  Express as a fraction and a decimal.   "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
